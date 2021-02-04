038aecb0b94f3ccc60220b964762ff5f956d685d
skychx <skychx@hotmail.com>
2021-02-04 23:06:10 +0800

    🏷️ 更新 ArrayQueue 类型

---
+++ 04-Queue/LinkedListQueue.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: skychx
+ * @Date: 2021-02-04 21:25:22
+ * @LastEditors: skychx
+ * @LastEditTime: 2021-02-04 22:38:34
+ * @FilePath: /Toy-Data-Structures/04-Queue/LinkedListQueue.ts
+ */
+import { ToyQueue } from './ToyQueue';
+import { LinkedNode } from '../02-LinkedList/ToyLinkedList';
