ca760dbc91d98f562a99a9d4598b331a9bdfcad3
skychx <skychx@hotmail.com>
2021-02-07 17:52:33 +0800

    💡 更新 ToyStack & ToyQueue 注释

--- 03-Stack/ToyStack.d.ts
+++ 03-Stack/ToyStack.d.ts
@@ -2,14 +2,17 @@
  * @Author: skychx
  * @Date: 2021-02-04 17:04:29
  * @LastEditors: skychx
- * @LastEditTime: 2021-02-04 17:54:47
+ * @LastEditTime: 2021-02-07 15:25:16
  * @FilePath: /Toy-Data-Structures/03-Stack/ToyStack.d.ts
  */
 export interface ToyStack<T> {
     getSize(): number;
     isEmpty(): boolean;
 
+    // 增
     push(e: T): void;
+    // 删
     pop(): T | null;
+    // 查
     peek(): T | null;
 }
--- 04-Queue/ToyQueue.d.ts
+++ 04-Queue/ToyQueue.d.ts
@@ -2,14 +2,17 @@
  * @Author: skychx
  * @Date: 2021-02-04 20:39:39
  * @LastEditors: skychx
- * @LastEditTime: 2021-02-04 21:10:36
+ * @LastEditTime: 2021-02-07 15:25:01
  * @FilePath: /Toy-Data-Structures/04-Queue/ToyQueue.d.ts
  */
 export interface ToyQueue<T> {
     getSize(): number;
     isEmpty(): boolean;
 
+    // 增
     enqueue(e: T): void; // 入队
+    // 删
     dequeue(): T | null; // 出队
+    // 查
     getFront(): T | null;
 }
