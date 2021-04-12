766dccf57e22528e52dd602f425f4cd17a0eb63a
skychx <skychx@hotmail.com>
2021-04-12 09:13:17 +0800

    ✨ ToyHeap use JavaScript Array

--- 08-Heap/ToyMaxHeap.ts
+++ 08-Heap/ToyMaxHeap.ts
@@ -2,36 +2,34 @@
  * @Author: skychx
  * @Date: 2021-02-08 16:25:17
  * @LastEditors: skychx
- * @LastEditTime: 2021-02-12 16:49:22
+ * @LastEditTime: 2021-04-12 09:02:21
  * @FilePath: /Toy-Data-Structures/08-Heap/ToyMaxHeap.ts
  */
 import { ToyHeap } from './ToyHeap';
 
-import { ToyArray } from '../01-Array/ToyArray';
-
 export class ToyMaxHeap<T> implements ToyHeap<T> {
-    private data: ToyArray<T>;
+    data: Array<T>;
 
-    constructor(params: number | T[]) {
+    constructor(params?: number | T[]) {
         // 如果传入数组，直接转为堆（堆化操作）
         if (Array.isArray(params)) {
-            this.data = new ToyArray<T>(params);
+            this.data = params;
             if(params.length !== 1) {
                 for (let i = this.parent(params.length - 1); i >= 0 ; i--) {
                     this.siftDown(i);
                 }
             }
         } else {
-            this.data = new ToyArray<T>();
+            this.data = [];
         }
     }
 
     getSize(): number {
-        return this.data.getSize();
+        return this.data.length;
     }
 
     isEmpty(): boolean {
-        return this.data.isEmpty();
+        return this.getSize() === 0;
     }
 
     // 寻找当前节点的父亲节点索引
@@ -53,21 +51,25 @@ export class ToyMaxHeap<T> implements ToyHeap<T> {
         return index * 2 + 2;
     }
 
+    private swap(i: number, j: number): void {
+        [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
+    }
+
     /** 增 **/
 
     push(e: T): void {
         // 插入后再整理堆的顺序
         this.data.push(e);
-        this.siftUp(this.data.getSize() - 1);
+        this.siftUp(this.getSize() - 1);
     }
 
     // 元素上浮
     private siftUp(i: number): void {
         while (
             i > 0 &&
-            (this.data.get(this.parent(i)) < this.data.get(i))
+            (this.data[this.parent(i)] < this.data[i])
         ) {
-            this.data.swap(i, this.parent(i));
+            this.swap(i, this.parent(i));
             i = this.parent(i);
         }
     }
@@ -76,12 +78,12 @@ export class ToyMaxHeap<T> implements ToyHeap<T> {
 
     // 寻找堆中的最大的元素
     peek(): T {
-        if(this.data.isEmpty()) {
+        if(this.isEmpty()) {
             throw new Error('Can not peek when heap is empty.');
         }
 
         // 最大元素就是数组的第一个元素
-        return this.data.get(0);
+        return this.data[0];
     }
 
     /** 删 **/
@@ -90,7 +92,10 @@ export class ToyMaxHeap<T> implements ToyHeap<T> {
     pop(): T {
         let res = this.peek();
 
-        this.data.swap(0, this.data.getSize() - 1);
+        // 把堆顶元素和最后一个元素交换位置
+        // 然后移除最后一个元素（时间复杂度为 O(1)）
+        // 然后从堆顶开始执行自上而下的堆化操作
+        this.swap(0, this.getSize() - 1);
         this.data.pop();
         this.siftDown(0);
 
@@ -100,25 +105,25 @@ export class ToyMaxHeap<T> implements ToyHeap<T> {
     // 元素下沉
     private siftDown(i: number): void {
         // 只有左孩子在数组内才能继续
-        while (this.leftChild(i) < this.data.getSize()) {
+        while (this.leftChild(i) < this.getSize()) {
             // 默认交换的是左孩子
             let j: number = this.leftChild(i);
 
             // 如果右孩子存在，且右孩子比左孩子大，我们就拿右孩子交换
             if (
-                j + 1 < this.data.getSize() &&
-                this.data.get(j + 1) > this.data.get(j)
+                j + 1 < this.getSize() &&
+                this.data[j + 1] > this.data[j]
             ) {
                 j++;
                 // j = this.rightChild(i);
             }
 
             // 如果 data[i] >= data[j]，说明交换到合适的位置了，直接停止下沉
-            if(this.data.get(i) >= this.data.get(j)) {
+            if(this.data[i] >= this.data[j]) {
                 break;
             }
 
-            this.data.swap(i, j);
+            this.swap(i, j);
             i = j;
         }
     }
@@ -129,7 +134,7 @@ export class ToyMaxHeap<T> implements ToyHeap<T> {
     replace(e: T): T {
         let res = this.peek();
 
-        this.data.set(0, e);
+        this.data[0] = e;
         this.siftDown(0);
 
         return res;
